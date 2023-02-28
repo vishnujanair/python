10ff2546e4c72d7235cf19de6214c7bb88351230
vishnujanair <126573175+vishnujanair@users.noreply.github.com>
2023-02-28 14:22:54 +0100

    Add files via upload

---
+++ count.py
@@ -0,0 +1,10 @@
+x=int(input("enter your mark"))
+if x>24:
+    print("You have passed the test")
+    
+    if x>90:
+        print("You have an A+")
+    else:
+        print("You dont have an A+")
+else:
+     print("Failed")
