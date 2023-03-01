1fab6bbbd532deb3c7eccbc027cac0bae9f7a90f
vishnujanair <126573175+vishnujanair@users.noreply.github.com>
2023-03-01 14:35:42 +0100

    Add files via upload

---
+++ append.py
@@ -0,0 +1,5 @@
+list_1=[]
+for i in range(5):
+    num=(input("enter string:"))
+    list_1.append(num)
+    print(list_1)
---
+++ for_upper.py
@@ -0,0 +1,4 @@
+name=input("enter string")
+for i in name:
+    if i.isupper():
+        print(i)
